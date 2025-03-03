045e01c5abef0691dff7ebaf561a459b01e63205
chloe <chloep2838@tuckahoeschools.org>
2025-03-03 03:52:17 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-let addition = document.querySelector(".addition")
-let subtraction = document.querySelector(".subtraction")
-let multiplication = document.querySelector(".multipilication")
-let division = document.querySelector(".division")
-let squareRoot = document.querySelector(".square-root")
-let equals = document.querySelector(".equals")
 let display = document.querySelector("#display")
 
+//appends numbers onto the display
 function appendNumber(newNumber) {
-   let currentValue = document.querySelector("#display").value;
-   document.querySelector("#display").value = currentValue + newNumber;
-   if (newNumber === '0' && display.value === '0') return;
+   if (display.value === "0" && newNumber === "0") return; //prevents multiple leading zeros
+   display.value += newNumber; //sets the value of the display to the number
 };
+
+//appends operations onto the display (+, -, /, *)
+function appendOperation(input) {
+      if (display.value === "" && (input === "*" || input === "/" || input === "+")) return; //prevents the equation from starting with an operate
+      display.value += input;
+};
+
+// calculates the result
+function calculate() {
+   try{
+      display.value = eval(display.value); //evaluates the value of the display
+
+      //displays the hidden image 
+      let image = document.getElementById("eureka");
+        image.style.display = "block";
+
+      //sets time limit on image so it is hidden again after 2 seconds
+        setTimeout(() => {
+         image.style.display = "none";
+     }, 2000); 
+   }
+   //catches errors and displays it 
+   catch(error){
+      display.value = "Error";
+   }
+   
+}
+
+// Clears the display 
+function clearDisplay() {
+   display.value = ""; 
+}
